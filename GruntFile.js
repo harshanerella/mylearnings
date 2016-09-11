5ae8c3438aecdbe57b89175caaf7bf403c68b110
harshanerella <nerellapriyank@gmail.com>
2016-09-11 23:45:48 +0530

    About Uglify

--- GruntFile.js
+++ GruntFile.js
@@ -9,11 +9,26 @@ module.exports = function(grunt) {
     		src: ['js/Main.js','js/Controller.js'],
     		dest: 'bin/Concat.js'
     	}
-    }
+    },
+      uglify:{
+        options:{
+           mangle: false
+        },
+         my_target:{
+          files:{
+             'bin/js/uglify.min.js' : ['bin/Concat.js']
+          }
+         }
+
+      }
+    
   });
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   //tasks
+  grunt.registerTask('JustJavascript', ['concat', 'uglify']);
+
   grunt.registerTask('default', ['concat']);
 
 };
